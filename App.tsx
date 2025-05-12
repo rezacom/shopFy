af2212c89587f7874f0bdaa7068505d332da4697
kamil <reza.shoja@utechia.com>
2025-05-12 16:55:32 +0330

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Alert, Button, Image, Platform, SafeAreaView, StyleSheet, StatusBar, View } from "react-native";
 export default function App() {
+  const handlePress = () => console.log("text press");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button
+        title="Click Me!"
+        onPress={() => {
+          Alert.alert("Title", "m=Messages", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ]);
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    gap: 20,
+    padding: 5,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  tinyLogo: {
+    width: 200,
+    height: 200,
+  },
+  logo: {
+    width: 66,
+    height: 58,
   },
 });
